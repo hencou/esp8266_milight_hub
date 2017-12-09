e8fb19a97bf9fd9df955ee9d43bdf77a4b02ddd3
hencou <henk@courtz.nl>
2017-12-09 14:07:37 +0100

    Added credentials.h to .gitignore

--- lib/ESP8266WebServer/src/ESP8266WebServer.h
+++ lib/ESP8266WebServer/src/ESP8266WebServer.h
@@ -33,7 +33,7 @@ enum HTTPUploadStatus { UPLOAD_FILE_START, UPLOAD_FILE_WRITE, UPLOAD_FILE_END,
 enum HTTPClientStatus { HC_NONE, HC_WAIT_READ, HC_WAIT_CLOSE };
 
 #define HTTP_DOWNLOAD_UNIT_SIZE 1460
-#define HTTP_UPLOAD_BUFLEN 20
+#define HTTP_UPLOAD_BUFLEN 2048
 #define HTTP_MAX_DATA_WAIT 1000 //ms to wait for the client to send the request
 #define HTTP_MAX_POST_WAIT 1000 //ms to wait for POST data to arrive
 #define HTTP_MAX_CLOSE_WAIT 2000 //ms to wait for the client to close the connection
@@ -129,7 +129,7 @@ template<typename T> size_t streamFile(T &file, const String& contentType){
     sendHeader("Content-Encoding", "gzip");
   }
   send(200, contentType, "");
-  return _currentClient.write(file, HTTP_DOWNLOAD_UNIT_SIZE);
+  return _currentClient.write(file);
 }
 
 protected:
